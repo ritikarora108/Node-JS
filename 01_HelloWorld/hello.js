9a76b691da5041a8a2c156180d11be7b5ebcfb65
Ritik Arora <aroraritik300@gmail.com>
2025-01-18 17:05:20 +0530

    Hello to NodeJs

---
+++ 01_HelloWorld/hello.js
@@ -0,0 +1,14 @@
+console.log("Hello World!")
+
+
+// Node js is a runtime environment for js
+
+// V8 engine with the modifications :
+// removed client side rendering functionalities (DOM related): document,window,navigator,alert
+
+// only server side rendering functionalities are there along with various different packages such as of cryptography for enc-decryption
+
+// ReferenceError: Keywords like window,document, alert are not defined
+// console.log(window) 
+// console.log(document)
+// alert('ok');
