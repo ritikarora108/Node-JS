1d10cb89cba042c9cb384f06a1247bbf521d252a
Ritik Arora <aroraritik300@gmail.com>
2025-01-22 22:50:53 +0530

    implement user management features with CRUD operations and update users.json format

--- 04_Project/index.js
+++ 04_Project/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import { createRequire } from "module"
+import fs from "fs"
 
 dotenv.config();
 
@@ -8,9 +9,17 @@ const require = createRequire(import.meta.url)
 const users = require("./users.json")
 
 
-
 const app = express();
-// console.log(users);
+// middleware - plugin
+app.use(express.urlencoded({ extended: false }))
+
+let prefix_id = Number(process.env.PREFIX_ID);
+
+function updateUsersData(users) {
+    fs.writeFile('./users.json', JSON.stringify(users), (err, data) => {
+        if (err) console.log(`Error: ${err}`);
+    })
+}
 
 app.get('/', (req, res) => {
     res.end('Home Page');
@@ -34,18 +43,50 @@ app.route('/api/users/:id').
     get((req, res) => {
         const id = Number(req.params.id);
         const user = users.find((user) => user.id === id);
-        return res.json(user);
+        return res.json(user??{status: "User not found"});
     }).patch((req, res) => {
         // Update users with id
-        return res.send({ status: pending });
+        const id = Number(req.params.id)
+        const body = req.body;
+        const userIndex = users.findIndex(user => user.id === id);
+        let idFieldExist = false;
+        for (const property in body) {
+            if (property === 'id') {
+                idFieldExist = true;
+                continue;
+            }
+            users[userIndex][property] = body[property];
+        }
+        updateUsersData(users);
+        let msg = "Users details updated successfully";
+        console.log(idFieldExist)
+        if (idFieldExist) {
+            if (Object.keys(body).length === 1) {
+                msg="id can't be updated"
+            } else {
+                msg="id can't be updated, but other user details have been updated!"
+            }
+        }
+        return res.json({...users[userIndex], status: msg });
     }).delete((req, res) => {
         // Delete users with id
-        return res.send({ status: pending });
+        const id = Number(req.params.id);
+        const userIndex = users.findIndex(user => user.id === id);
+
+        users.splice(userIndex, 1);
+        updateUsersData(users)
+        return res.json({ status: "User deleted successfully" });
     });
 
 app.post('/api/users', (req, res) => {
     // Create a user in db with the available form data
-    return res.send({status:pending})
+    const body = req.body;
+    users.push({ id: prefix_id + 1, ...body });
+    prefix_id++;
+    process.env.PREFIX_ID = prefix_id;
+    
+    updateUsersData(users);
+    return res.json({ id:prefix_id, ...body , status: "User added successfully" });
 })
 
 app.listen(process.env.PORT, () => console.log(`Server started on port ${process.env.PORT}`))
