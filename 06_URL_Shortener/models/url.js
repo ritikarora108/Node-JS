9d6efa8bd25953a534290c05c01efba890b3f3da
Ritik Arora <aroraritik300@gmail.com>
2025-01-24 18:58:43 +0530

    feat: add createdBy field to URL model for user association

--- 06_URL_Shortener/models/url.js
+++ 06_URL_Shortener/models/url.js
@@ -10,7 +10,11 @@ const urlSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    visitHistory: [ {timestamp: Number}],
+    visitHistory: [{ timestamp: Number }],
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+    }
 }, { timestamps: true });
 
 const URL = mongoose.model("url", urlSchema);
