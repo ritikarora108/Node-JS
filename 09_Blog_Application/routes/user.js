d9ff0d3701c14680b912b8603268d0a3055815c4
Ritik Arora <aroraritik300@gmail.com>
2025-01-28 20:29:46 +0530

    refactor: simplify user authentication routes by delegating to controller functions

--- 09_Blog_Application/routes/user.js
+++ 09_Blog_Application/routes/user.js
@@ -3,54 +3,22 @@ import User from "../models/user.js";
 import authentication from "../services/authentication.js";
 import upload from "../services/fileUpload.js";
 
+import userController from "../controllers/user.js"
+
+const { handleUserSignIn, handleUserSignUp } = userController;
+
 const router = Router();
 
 router.get('/signin', (req, res) => {
     return res.render('signin');
 })
 
-router.post('/signin', async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        const token = await User.matchPasswordAndGenerateToken(email, password);
-        if(!token) throw new Error('JWT malformed')
-        res.cookie("token", token);
-
-        console.log(token);
-        
-        return res.redirect('/');
-    } catch (error) {
-        console.log(error);
-        res.render('signin', {
-            error: "Incorrect Email or Password!"
-        })
-    }
-})
+router.post('/signin', handleUserSignIn)
 
 router.get('/signup', (req, res) => {
     return res.render('signup');
 })
-router.post('/signup', upload.single('profileImage'), async (req, res) => {
-    try {
-        const { fullName, email, password } = req.body;
-
-        // Prepare the data to be inserted
-        const userData = {
-            fullName,
-            email,
-            password,
-            profileImageURL: req.file ? `/uploads/${req.file.filename}` : undefined  // If file exists, use the path
-        };
-
-        // Create the user with the prepared data
-        await User.create(userData);
-
-        return res.redirect('/user/signin');
-    } catch (error) {
-        console.log(error);
-    }
-
-})
+router.post('/signup', upload.single('profileImage'), handleUserSignUp)
 
 
 router.get('/logout', (req, res) => {
