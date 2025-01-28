c79b52e3f9069137fca54e3eeef2f2b59dcf534f
Ritik Arora <aroraritik300@gmail.com>
2025-01-28 20:29:34 +0530

    refactor: extract blog route handlers into controller functions for better organization

--- 09_Blog_Application/routes/blog.js
+++ 09_Blog_Application/routes/blog.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import upload from "../services/fileUpload.js"
-
 import Blog from "../models/blog.js"
 import Comment from "../models/comment.js"
+import blogController from "../controllers/blog.js"
+
+const { handleCreateNewBlog, handleGetBlogDetails, handlePostComment } = blogController;
 
 const router = Router();
 
@@ -12,42 +14,10 @@ router.get('/add-new', (req, res) => {
     })
 });
 
-router.post('/', upload.single('coverImage'), async (req, res) => {
-    const blog = await Blog.create({
-        title: req.body.title,
-        body: req.body.body,
-        coverImageURL: `/uploads/${req.file.filename}`,
-        createdBy: req.user._id,
-    })
-    res.redirect(`/blog/${blog._id}`);
-})
-router.get('/:blogId', async (req, res) => {
-    try {
-        const blog = await Blog.findById(req.params.blogId).populate("createdBy");
-        const comments = await Comment.find({ blogId: req.params.blogId }).populate("createdBy");
+router.post('/', upload.single('coverImage'), handleCreateNewBlog)
+router.get('/:blogId', handleGetBlogDetails)
 
-        // console.log(comments);
-        return res.render('blog', {
-            user: req.user,
-            blog,
-            comments,
-        })    
-    } catch (error) {
-        console.log(error)
-    }
-
-})
-
-router.post('/comment/:blogId', async (req, res) => {
-    await Comment.create({
-        content: req.body.content,
-        blogId: req.params.blogId,
-        createdBy: req.user._id,
-    })
-
-    return res.redirect(`/blog/${req.params.blogId}`);
-
-})
+router.post('/comment/:blogId', handlePostComment)
 
 
 export default router;
